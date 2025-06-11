e34ed83e82c0cc6e8e864fdc70ca8e486c10bb56
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-11 17:25:39 +0900

    Calculator

--- src/Main.java
+++ src/Main.java
@@ -4,57 +4,58 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("돈을 넣어주세요.");
-        int money = scanner.nextInt();
-
         while(true){
-            System.out.println("---- 메뉴 선택 ----");
-            System.out.println("1. 콜라   (1,200원)");
-            System.out.println("2. 사이다  (1,200원)");
-            System.out.println("3. 물     (800원)");
-            System.out.println("4. 커피   (1,500원)");
-            System.out.println("0. 자판기 종료");
+            double result = 0;
+            boolean valid = true;
 
-            int choice = scanner.nextInt();
-            String item = "";
-            int price = 0;
+            System.out.println("=====계산기=====");
 
-            switch (choice){
-                case 1:
-                    item = "콜라";
-                    price = 1200;
+            System.out.print("첫 번째 피연산자 입력 : ");
+            double operand1 = scanner.nextDouble();
+
+            System.out.print("두 번째 피연산자 입력 : ");
+            double operand2 = scanner.nextDouble();
+
+            System.out.print("연산자 입력 : ");
+            char operator = scanner.next().charAt(0);
+
+            System.out.println();
+
+            switch (operator){
+                case '+':
+                    result = operand1 + operand2;
                     break;
-                case 2:
-                    item = "사이다";
-                    price = 1200;
+                case '-':
+                    result = operand1 - operand2;
                     break;
-                case 3:
-                    item = "물";
-                    price = 800;
+                case '*':
+                    result = operand1 * operand2;
                     break;
-                case 4:
-                    item = "커피";
-                    price = 1500;
+                case '/':
+                    // 0으로 나누지 않을 경우만 계산 수행
+                    if (operand2 != 0){
+                        result = operand1 / operand2;
+                    } else {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        valid = false;
+                    }
                     break;
-                case 0:
-                    System.out.println("자판기를 종료합니다. 남은 돈을 가져가주세요." + money);
-                    return;
-                    // 종료. 남은 돈 환불
                 default:
-                    System.out.println("정해진 메뉴 중에서 선택 해주세요");
+                    System.out.println("사칙연산 중에서 선택 해주세요");
+                    valid = false;
                     break;
             }
 
             while(true){
-                if (money < price){
-                    System.out.println("금액이 부족합니다. 추가 투입 해주세요.");
-                    int addMoney = scanner.nextInt();
-                    money = money + addMoney;
-                } else{
-                    money -= price;
-                    System.out.printf("%s이(가) 나왔습니다. 잔돈은 %d원 입니다.\n", item, money);
+                if (valid){
+                    System.out.printf("결과는 %.2f입니다. \n다시 하려면 Y, 종료시 아무키나 입력해주세요.", result);
+                } else System.out.println("다시 하려면 Y, 종료시 아무키나 입력해주세요.");
+
+                char a = scanner.next().charAt(0);
+
+                if(a == 'Y' || a == 'y'){
                     break;
-                }
+                } else return;
             }
         }
 
