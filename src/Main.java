ef4e1cf926cce7313a2f52525a68650fce7ca6c5
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-11 11:13:42 +0900

    print type

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,14 @@
-class Car{
-    String brand;
-    int year;
-    String model;
-    String color = "";
-
-    public Car(String brand, int year, String model){
-        this.brand = brand;
-        this.year = year;
-        this.model = model;
-    }
-
-    public Car(String brand, int year, String model, String color){
-        this.brand = brand;
-        this.year = year;
-        this.model = model;
-        this.color = color;
-    }
-
-    void displayInfo(){
-        String colorPrint = this.color.isEmpty() ? "" : this.color + "색상이 이쁩니다.";
-
-        System.out.println(
-                brand + "사의 " + year + "년식 " + model + "입니다. "
-                        + colorPrint);
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("hyundai", 2025, "genesis g90",  "Black");
-        car1.displayInfo();
-        Car car2 = new Car("hyundai", 2025, "genesis gv80");
-        car2.displayInfo();
+        String name = "Son";
+        Integer backNumber = 7;         // int -> Integer (박싱)
+        int num = 1;
+
+        int sum = num + backNumber;     // Integer -> (언박싱)
+
+        System.out.println(backNumber + num);
+        System.out.println(((Object) sum).getClass().getName());
+        System.out.println((name + backNumber).getClass().getName());
+        System.out.println((backNumber).getClass().getName());
     }
 }
