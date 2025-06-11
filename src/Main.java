714775c3518957d037e9ab35e1597422a656bd5c
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-11 13:35:06 +0900

    scanner method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name = "Alice";
-        int age = 28;
-        double height = 164.52;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Name: %s, Age: %d, Height: %.1fcm", name, age, height);
+        String name = scanner.nextLine();
+        String word = scanner.next();
+        int Int = scanner.nextInt();
+        double Double = scanner.nextDouble();
+        char Char = scanner.next().charAt(0);   // n번째 단어
+
+        System.out.println("이름 : " + name);
+        System.out.println("단어 : " + word);
+        System.out.println("정수 : " + Int);
+        System.out.println("실수 : " + Double);
+        System.out.println("문자 : " + Char);
     }
 }
