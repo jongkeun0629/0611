d949cd551752c91b6a998dccc226ae9bdef589e0
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-11 10:24:58 +0900

    Car Class Constructor

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
-class Person{
-    String name;
-    int age;
+class Car{
+    String brand;
+    int year;
+    String model;
+    String color = "";
 
-    public Person(){}
+    public Car(String brand, int year, String model){
+        this.brand = brand;
+        this.year = year;
+        this.model = model;
+    }
 
-    public Person(String name, int age){
-        this.name = name;
-        this.age = age;
+    public Car(String brand, int year, String model, String color){
+        this.brand = brand;
+        this.year = year;
+        this.model = model;
+        this.color = color;
+    }
+
+    void displayInfo(){
+        String colorPrint = this.color.isEmpty() ? "" : this.color + "색상이 이쁩니다.";
+
+        System.out.println(
+                brand + "사의 " + year + "년식 " + model + "입니다. "
+                        + colorPrint);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Person p1 = new Person("이종근", 26);
-        Person p2 = new Person();
-
-        System.out.println(p1.name + ", " + p1.age);
+        Car car1 = new Car("hyundai", 2025, "genesis g90",  "Black");
+        car1.displayInfo();
+        Car car2 = new Car("hyundai", 2025, "genesis gv80");
+        car2.displayInfo();
     }
 }
